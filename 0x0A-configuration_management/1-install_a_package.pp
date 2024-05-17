65726ee34a2bef36e352e197035489e8f422a056
Joedgan12 <joepackins@gmail.com>
2024-05-17 11:57:28 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#Using Puppet  to, install puppet-lint.
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
